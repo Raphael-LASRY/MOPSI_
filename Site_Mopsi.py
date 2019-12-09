4888cdd4912c0f849fa26e0a587e016a185d5fb2
Raphael LASRY <raphaellasry1@hotmail.fr>
2019-12-09 13:10:32 +0100

    utiliser les chemis relatifs

--- Site_Mopsi.py
+++ Site_Mopsi.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os 
 
-op = pd.read_csv("C:/Users/pc/Desktop/Enpc/2A/MOPSI/MOPSI/MOPSI/Operations.csv", header = None, index_col = None, sep=",")
+op = pd.read_csv("Operations.csv", header = None, index_col = None, sep=",")
 
 test = True
 Noms_utilisateurs = 0
@@ -16,7 +16,7 @@ Noms_utilisateurs -= 1
 for nom in range (1,Noms_utilisateurs):
     Names.append(op[nom][0])
 
-op = pd.read_csv("C:/Users/pc/Desktop/Enpc/2A/MOPSI/MOPSI/MOPSI/Operations.csv", index_col = None, sep=",")
+op = pd.read_csv("Operations.csv", index_col = None, sep=",")
 
 L=[["NAME","VALUE"]]
 for name in Names:
@@ -25,12 +25,14 @@ print(L)
    
     
 Ldf = pd.DataFrame(L)
-Ldf.to_csv("C:/Users/pc/Desktop/Enpc/2A/MOPSI/MOPSI/MOPSI/Dettes.csv", index=False, header=False, sep=",")
+if (os.path.exists("Dettes.csv")):
+    os.remove("Dettes.csv")
+Ldf.to_csv("Dettes.csv", index=False, header=False, sep=",")
 
-if (os.path.exists("C:/Users/pc/Desktop/Enpc/2A/MOPSI/MOPSI/MOPSI/Results.csv")):
-    os.remove("C:/Users/pc/Desktop/Enpc/2A/MOPSI/MOPSI/MOPSI/Results.csv")
-if (os.path.exists("C:/Users/pc/Desktop/Enpc/2A/MOPSI/MOPSI/MOPSI/Exchanges.csv")):
-    os.remove("C:/Users/pc/Desktop/Enpc/2A/MOPSI/MOPSI/MOPSI/Exchanges.csv")
+if (os.path.exists("Results.csv")):
+    os.remove("Results.csv")
+if (os.path.exists("Exchanges.csv")):
+    os.remove("Exchanges.csv")
     
-os.system("glpsol -m C:/Users/pc/Desktop/Enpc/2A/MOPSI/MOPSI/MOPSI/TricountCalculMin1.MOD")
-os.system("glpsol -m C:/Users/pc/Desktop/Enpc/2A/MOPSI/MOPSI/MOPSI/TricountCalculFlow1.MOD")
+os.system("glpsol -m TricountCalculMin1.MOD")
+os.system("glpsol -m TricountCalculFlow1.MOD")
